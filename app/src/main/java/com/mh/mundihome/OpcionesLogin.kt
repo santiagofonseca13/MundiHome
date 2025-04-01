115a7d484f5805fa0f7392533fafdc039e7f5ec0
santiagofonseca13 <129698818+santiagofonseca13@users.noreply.github.com>
2025-04-01 18:59:03 -0500

    login
    video 9

--- app/src/main/java/com/mh/mundihome/OpcionesLogin.kt
+++ app/src/main/java/com/mh/mundihome/OpcionesLogin.kt
@@ -1,20 +1,25 @@
 package com.mh.mundihome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mh.mundihome.Opciones_Login.Login_email
+import com.mh.mundihome.databinding.ActivityOpcionesLoginBinding
 
 class OpcionesLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOpcionesLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_opciones_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityOpcionesLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.IngresarEmail.setOnClickListener{
+            startActivity(Intent(this@OpcionesLogin, Login_email::class.java))
         }
     }
 }
