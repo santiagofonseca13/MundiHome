c2e310d81e09588df24e11549960844366b35dfe
santiagofonseca13 <129698818+santiagofonseca13@users.noreply.github.com>
2025-04-02 10:20:53 -0500

    Revert "Parte 13"
    This reverts commit c784b0da9594acfc742550c58e83d87fadad3ed9.

--- app/src/main/java/com/mh/mundihome/OpcionesLogin.kt
+++ app/src/main/java/com/mh/mundihome/OpcionesLogin.kt
@@ -1,11 +1,7 @@
 package com.mh.mundihome
 
-import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Patterns
-import android.widget.Toast
-import androidx.activity.ComponentDialog
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,7 +14,6 @@ class OpcionesLogin : AppCompatActivity() {
     private lateinit var binding: ActivityOpcionesLoginBinding
 
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +22,6 @@ class OpcionesLogin : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         comprobarSesion()
-
-        progressDialog = ProgressDialog(this)
-        progressDialog.setTitle("Espere por favor")
-        progressDialog.setCanceledOnTouchOutside(false)
-
-        binding.BtnIngresar.setOnClickListener {
-            validarInfo()
-        }
 
         binding.TxtRegistrarme.setOnClickListener {
             startActivity(Intent(this@OpcionesLogin, Registro_email::class.java))
@@ -47,52 +34,4 @@ class OpcionesLogin : AppCompatActivity() {
             finishAffinity()
         }
     }
-    private var email = ""
-    private var password = ""
-    private fun validarInfo() {
-        email = binding.EtEmail.text.toString().trim()
-        password = binding.EtPassword.text.toString().trim()
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            binding.EtEmail.error = "Correo invalido"
-            binding.EtEmail.requestFocus()
-        }
-        else if (email.isEmpty()){
-            binding.EtEmail.error = "Ingrese un Correo"
-            binding.EtEmail.requestFocus()
-        }
-        else if (password.isEmpty()){
-            binding.EtPassword.error = "Ingrese contraseña"
-            binding.EtPassword.requestFocus()
-        }else{
-            loginUsuario()
-        }
-    }
-
-    private fun loginUsuario(){
-        progressDialog.setMessage("Ingresando")
-        progressDialog.show()
-
-        firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnSuccessListener {
-                progressDialog.dismiss()
-                startActivity(Intent(this, MainActivity::class.java))
-                finishAffinity()
-                Toast.makeText(
-                    this,
-                    "Bienvenido(a)",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            .addOnFailureListener { e->
-                progressDialog.dismiss()
-                Toast.makeText(
-                    this,
-                    "No se pudo iniciar sesión debido a ${e.message}",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-    }
 }
-
-
