ad78f2bf2751c3e8842c77b28656a17912ff89ee
santiagofonseca13 <129698818+santiagofonseca13@users.noreply.github.com>
2025-04-01 20:04:45 -0500

    Mejoras del Login
    Mejoras del Login

--- app/src/main/java/com/mh/mundihome/OpcionesLogin.kt
+++ app/src/main/java/com/mh/mundihome/OpcionesLogin.kt
@@ -6,7 +6,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.mh.mundihome.Opciones_Login.Login_email
 import com.mh.mundihome.databinding.ActivityOpcionesLoginBinding
 
 class OpcionesLogin : AppCompatActivity() {
@@ -17,9 +16,5 @@ class OpcionesLogin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityOpcionesLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.IngresarEmail.setOnClickListener{
-            startActivity(Intent(this@OpcionesLogin, Login_email::class.java))
-        }
     }
 }
