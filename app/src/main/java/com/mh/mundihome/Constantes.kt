71803208dc0a2379fcfe2e02e3d7bf324883fa47
EdisonStive <edison.gonzalez@uniminuto.edu.co>
2025-04-02 22:16:41 -0500

    parte 16
    Acceder informacion en la cuenta y mostrar imagen de perfil.

--- app/src/main/java/com/mh/mundihome/Constantes.kt
+++ app/src/main/java/com/mh/mundihome/Constantes.kt
@@ -1,9 +1,20 @@
 package com.mh.mundihome
 
+import android.icu.text.DateFormat
+import java.util.Calendar
+import java.util.Locale
+
 object Constantes {
 
     fun obtenetTiempoDis() : Long{
         return System.currentTimeMillis()
+    }
+
+    fun obtenerFecha(tiempo : Long) : String{
+        val calendario = Calendar.getInstance(Locale.ENGLISH)
+        calendario.timeInMillis = tiempo
+
+        return android.text.format.DateFormat.format("dd/MM/yyyy", calendario).toString()
     }
 
 }
