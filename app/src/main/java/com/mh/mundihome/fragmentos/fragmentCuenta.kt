e121c0b39aa74c407d42cf1731c91ad1c5e2d733
EdisonStive <edison.gonzalez@uniminuto.edu.co>
2025-04-02 22:40:24 -0500

    Revert "parte 16"
    This reverts commit 71803208dc0a2379fcfe2e02e3d7bf324883fa47.

--- app/src/main/java/com/mh/mundihome/fragmentos/fragmentCuenta.kt
+++ app/src/main/java/com/mh/mundihome/fragmentos/fragmentCuenta.kt
@@ -7,20 +7,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.mh.mundihome.Constantes
 import com.mh.mundihome.OpcionesLogin
 import com.mh.mundihome.R
 import com.mh.mundihome.databinding.FragmentCuentaBinding
 import java.net.Authenticator
-
-
 
 
 class fragmentCuenta : Fragment() {
@@ -45,75 +36,11 @@ class fragmentCuenta : Fragment() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        leerInfo()
-
         binding.BtnCerrarSesiN.setOnClickListener {
             firebaseAuth.signOut()
             startActivity(Intent(mContext, OpcionesLogin::class.java))
             activity?.finishAffinity()
         }
-    }
-
-    private fun leerInfo() {
-        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
-        ref.child("${firebaseAuth.uid}")
-            .addValueEventListener(object : ValueEventListener{
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    val nombres = "${snapshot.child("nombres").value}"
-                    val email = "${snapshot.child("email").value}"
-                    val imagen = "${snapshot.child("urlImagenPerfil").value}"
-                    val f_nac = "${snapshot.child("fecha_nac").value}"
-                    var tiempo = "${snapshot.child("tiempo").value}"
-                    val telefono = "${snapshot.child("telefono").value}"
-                    val codTelefono = "${snapshot.child("codigoTelefono").value}"
-                    val proveedor = "${snapshot.child("proveedor").value}"
-
-                    val cod_tel = codTelefono+telefono
-
-                    if (tiempo == "null"){
-                        tiempo = "0"
-                    }
-
-                    val for_tiempo = Constantes.obtenerFecha(tiempo.toLong())
-
-                    //Seteamos informacion
-                    binding.TvEmail.text = email
-                    binding.TvNombres.text = nombres
-                    binding.TvNacimiento.text = f_nac
-                    binding.TvTelefono.text = cod_tel
-                    binding.TvMiembro.text = for_tiempo
-
-                    //Seteo de la imagen
-                    try {
-                        Glide.with(mContext)
-                            .load(imagen)
-                            .placeholder(R.drawable.login)
-                            .into(binding.TvPerfil)
-                    }catch (e:Exception){
-                        Toast.makeText(
-                            mContext,
-                            "${e.message}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-
-                    if (proveedor == "Email"){
-                        val esVerificado = firebaseAuth.currentUser!!.isEmailVerified
-                        if (esVerificado){
-                            binding.TvEstadoCuenta.text = "Verificado"
-                        }else{
-                            binding.TvEstadoCuenta.text = "No verificado"
-                        }
-                    }else{
-                        binding.TvEstadoCuenta.text = "Verificado"
-                    }
-
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-            })
     }
 
 }
