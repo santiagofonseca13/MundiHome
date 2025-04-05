4bf150cef679d211523cbbb801bc77a4e0f3929d
santiagofonseca13 <129698818+santiagofonseca13@users.noreply.github.com>
2025-04-04 20:04:57 -0500

    Parte 24
    Se generan las listas en el apartado de crear anuncios por ejemplo Tipo Inmueble (Apartamento,  Finca, Casa, Local)

--- app/src/main/java/com/mh/mundihome/Anuncios/CrearAnuncio.kt
+++ app/src/main/java/com/mh/mundihome/Anuncios/CrearAnuncio.kt
@@ -1,15 +1,62 @@
 package com.mh.mundihome.Anuncios
 
+import android.app.ProgressDialog
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.mh.mundihome.Adaptadores.AdaptadorImagenSeleccionada
+import com.mh.mundihome.Constantes
 import com.mh.mundihome.R
+import com.mh.mundihome.databinding.ActivityCrearAnuncioBinding
+import com.mh.mundihome.modelo.ModeloImagenSeleccionada
 
 class CrearAnuncio : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCrearAnuncioBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
+    private var imagenUri:  Uri? = null
+
+    private lateinit var  imagenesArrayList: ArrayList<ModeloImagenSeleccionada>
+    private lateinit var  adaptadorImagenSeleccionada: AdaptadorImagenSeleccionada
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_crear_anuncio)
+        binding = ActivityCrearAnuncioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        val  adaptadorTipoInmueble = ArrayAdapter(this, R.layout.item_tipo_inmueble, Constantes.tipo_inmueble)
+        binding.TipoInmueble.setAdapter(adaptadorTipoInmueble)
+
+        val  adaptadorCiudad = ArrayAdapter(this, R.layout.item_ciudad, Constantes.ciudad)
+        binding.Ciudad.setAdapter(adaptadorCiudad)
+
+        val  adaptadorEstrato = ArrayAdapter(this, R.layout.item_estrato, Constantes.estrato)
+        binding.Estrato.setAdapter(adaptadorEstrato)
+
+        val  adaptadorDormitorios = ArrayAdapter(this, R.layout.item_dormitorios, Constantes.dormitorios)
+        binding.Dormitorios.setAdapter(adaptadorDormitorios)
+
+        val  adaptadorBaños = ArrayAdapter(this, R.layout.item_banos, Constantes.banos)
+        binding.BaOs.setAdapter(adaptadorBaños)
+
+        val  adaptadorEstacionamiento = ArrayAdapter(this, R.layout.item_estacionamiento, Constantes.estacionamiento)
+        binding.Estacionamiento.setAdapter(adaptadorEstacionamiento)
+
+        val  adaptadorMascotas = ArrayAdapter(this, R.layout.item_mascotas, Constantes.marcotas)
+        binding.AceptaMascotas.setAdapter(adaptadorMascotas)
+
+        val  adaptadorAdministracion = ArrayAdapter(this, R.layout.item_administracion, Constantes.administracion)
+        binding.IncluyeAdministracion.setAdapter(adaptadorAdministracion)
     }
 }
--- app/src/main/java/com/mh/mundihome/Constantes.kt
+++ app/src/main/java/com/mh/mundihome/Constantes.kt
@@ -3,12 +3,87 @@ package com.mh.mundihome
 import android.icu.text.DateFormat
 import java.util.Calendar
 import java.util.Locale
+import kotlin.arrayOf
 
 object Constantes {
 
     fun obtenetTiempoDis() : Long{
         return System.currentTimeMillis()
     }
+
+    const val  anuncio_disponible = "Disponible"
+    const val anuncio_nodisponible = "No disponible / Vendido"
+
+    val tipo_inmueble = arrayOf(
+        "Apartamento",
+        "Finca",
+        "Casa",
+        "Local comercial",
+    )
+
+    val ciudad = arrayOf(
+        "Zipaquira"
+    )
+
+    val estrato = arrayOf(
+        "N/A",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10"
+    )
+
+    val dormitorios = arrayOf(
+        "N/A",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10"
+    )
+
+    val banos = arrayOf(
+        "N/A",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10"
+    )
+
+    val estacionamiento = arrayOf(
+        "N/A",
+        "Si",
+        "No"
+    )
+
+    val marcotas = arrayOf(
+        "N/A",
+        "Si",
+        "No"
+    )
+
+    val administracion = arrayOf(
+        "N/A",
+        "Si",
+        "No"
+    )
 
     fun obtenerFecha(tiempo : Long) : String{
         val calendario = Calendar.getInstance(Locale.ENGLISH)
