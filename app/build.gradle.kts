21c6dcd15b432d346d43bd6db9d380ddf0bef5dd
santiagofonseca13 <129698818+santiagofonseca13@users.noreply.github.com>
2025-04-16 17:13:58 -0500

    .
    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.mh.mundihome"
-    compileSdk = 35
+    namespace = "com.redsystemstudio.appcomprayventa"
+    compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.mh.mundihome"
-        minSdk = 29
-        targetSdk = 35
+        applicationId = "com.redsystemstudio.appcomprayventa"
+        minSdk = 24
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -28,43 +28,41 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
 
     buildFeatures{
         viewBinding = true
     }
+
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation("com.google.firebase:firebase-analytics:22.4.0")
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.navigation.fragment.ktx)
-    implementation(libs.navigation.ui.ktx)
-    implementation(libs.firebase.auth.ktx)
-    testImplementation(libs.junit)
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    //implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
+    implementation ("com.google.android.libraries.places:places:3.1.0")
     implementation ("com.hbb20:ccp:2.7.0")
-    implementation("com.google.firebase:firebase-storage")
-    implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.android.libraries.places:places:4.1.0")
-    implementation("com.google.firebase:firebase-database")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("com.github.chrisbanes:PhotoView:2.3.0")
+    implementation ("com.google.firebase:firebase-messaging-ktx:23.3.1")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.android.volley:volley:1.2.1")
+
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,6 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -16,8 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://www.jitpack.io")
     }
 }
 
-rootProject.name = "mundihome"
+rootProject.name = "App Compra y Venta"
 include(":app")
